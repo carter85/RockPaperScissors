a15b428fcfb54afc1e22e6a5aefb4d6e7e08e4f5
carter85 <caseycarter8520@gmail.com>
2022-03-25 11:09:43 -0500

    started writing code

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const playArr = ['Rock','Paper','Scissors'];
+//function play() {
+    
+function playerSelection () {
+    str = prompt("Choose your play: (R)ock, (P)aper, or (S)cissors:");
+    let lowStr = str.toLowerCase();
+    console.log(lowStr);
+    //if ( !== )
+}
+
+
+computerSelection = () => computerPlay();
+
+//if (playerSelection === "rock" && computerSelection === "rock")
+//}
+
+/* const computerPlay =()=> {
+    let num = Math.floor(Math.random() * 3);
+    return playArr[num];
+} */
