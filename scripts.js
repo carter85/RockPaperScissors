55a425d036df52e55e3472d562d3e7c9f2be054d
carter85 <caseycarter8520@gmail.com>
2022-03-25 12:06:49 -0500

    code working for single game currently

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
-const playArr = ['Rock','Paper','Scissors'];
-//function play() {
-    
-function playerSelection () {
-    str = prompt("Choose your play: (R)ock, (P)aper, or (S)cissors:");
-    let lowStr = str.toLowerCase();
-    console.log(lowStr);
-    //if ( !== )
+const playArr = ['rock', 'paper', 'scissors'];
+
+// function gets input from player, loops question if answered incorrectly,
+// converts answer to lowercase and to word for comparisons
+function playerSelection() {
+    let input = prompt("Choose your play: (R)ock, (P)aper, or (S)cissors, first letter only:").toLowerCase();
+
+    while (input !== 'r' && input !== 'p' && input !== 's') {
+        input = prompt("Invalid, please type the first letter only:").toLowerCase();
+    }
+    if (input === 'r') input = 'rock';
+    if (input === 's') input = 'scissors';
+    if (input === 'p') input = 'paper';
+    return input;
 }
 
+function computerPlay() {
+  let num = Math.floor(Math.random() * 3);
+  return playArr[num];
+}
 
-computerSelection = () => computerPlay();
+function play() {
+  let userChoice = playerSelection();
+  let compChoice = computerPlay();
+  console.log(userChoice + " " + compChoice);
 
-//if (playerSelection === "rock" && computerSelection === "rock")
-//}
+  if ((userChoice === "rock" && compChoice === "rock") ||
+    (userChoice === "paper" && compChoice === "paper") ||
+    (userChoice === "scissors" && compChoice === "scissors")) {
+    return "It's a tie.";
+  } else if ((userChoice === "rock" && compChoice === "scissors") ||
+    (userChoice === "paper" && compChoice === "rock") ||
+    (userChoice === "scissors" && compChoice === "paper")) {
+    return "Player wins."
+  } else if ((userChoice === "paper" && compChoice === "scissors") ||
+    (userChoice === "rock" && compChoice === "paper") ||
+    (userChoice === "scissors" && compChoice === "rock")) {
+    return "Computer Wins."
+  }
 
-/* const computerPlay =()=> {
-    let num = Math.floor(Math.random() * 3);
-    return playArr[num];
-} */
+}
+//playerSelection === "" && compChoice === ""
